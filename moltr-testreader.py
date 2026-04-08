5b209b14ada17d4dacb02fd61f393447b54daed5
Gašper Mali <gasper.mali9@gmail.com>
2026-04-08 09:24:40 +0200

    csv_to_pickle

--- moltr-testreader.py
+++ moltr-testreader.py
@@ -9,20 +9,32 @@ def stringify(x: list) -> str:
     return string
 
 
-with open('../../../Downloads/minute_full_station_merged.csv', encoding='utf-8') as f:
+def csv_to_pickle():
+    MIO = 1000000
 
-    # attribute names
-    headers = f.readline().split(',')
+    with open('../../../Downloads/minute_full_station_merged.csv', encoding='utf-8') as f:
 
-    # elements (lines)
-    content = []
-    for line in f:
-        line = line.strip().split(',')
-        content.append(line)
-    
-    # create table
-    table = tabulate(content, headers=headers)
+        # attribute names
+        headers = f.readline().split(',')
 
-    # create dataframe
-    df = pd.DataFrame(content, columns=headers)
-    print(df)
+        # elements (lines)
+        content = []
+        print('Reading dataset (0/21) ... ', end='', flush=True)
+        for i, line in enumerate(f):
+            line = line.strip().split(',')
+            content.append(line)
+
+            if i % MIO == 0:
+                print(i // MIO, end=' ', flush=True)
+        
+        print('21\nReading finished (21/21) - all 20.4 million lines read')
+
+        # create table
+        #table = tabulate(content, headers=headers)
+
+        # create dataframe
+        df = pd.DataFrame(content, columns=headers)
+        df.to_pickle('./data/minute_full_station_merged.pkl')
+        print(df.head(20))
+
+csv_to_pickle()
