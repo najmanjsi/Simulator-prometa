c7814963ef49711df94c85f862c39fa0090c8465
Gašper Mali <gasper.mali9@gmail.com>
2026-04-08 15:30:59 +0200

    Final commit of the day
    some statistics and a new file for counters' locations

--- moltr-testreader.py
+++ moltr-testreader.py
@@ -1,5 +1,7 @@
 from tabulate import tabulate
 import pandas as pd
+import os
+import sys
 
 
 def stringify(x: list) -> str:
@@ -9,32 +11,94 @@ def stringify(x: list) -> str:
     return string
 
 
-def csv_to_pickle():
-    MIO = 1000000
+def csv_to_pickle(filepath: str, outputpath: str = None):
 
-    with open('../../../Downloads/minute_full_station_merged.csv', encoding='utf-8') as f:
+    MIO = 1_000_000
+    BYTES_FOR_A_NUMBER = 182_943_047
+
+    # if the output path is not specified, set it to './data/filename.pkl'
+    if outputpath is None:
+        outputpath = './data/' + filepath.split('/')[-1].split('.')[0]
+
+    # totally unnecessary stuff for the progress 'bar'
+    filesize = os.path.getsize(filepath)
+    #numbers = filesize // BYTES_FOR_A_NUMBER + 1
+
+    number_of_lines = -1
+
+    # '../../../Downloads/minute_full_station_merged.csv'
+    with open(filepath, encoding='utf-8') as f:
 
         # attribute names
-        headers = f.readline().split(',')
+        headers = f.readline().strip().split(',')
 
         # elements (lines)
         content = []
-        print('Reading dataset (0/21) ... ', end='', flush=True)
+
+        # some estimates for the progress 'bar'
+        first_line = f.readline()
+        line_length = sys.getsizeof(first_line)
+        print(line_length)
+        print(len(first_line) * 2)
+        content.append(first_line.strip().split(','))
+        number_of_lines = filesize // (len(first_line) * 2)
+        numbers = number_of_lines // MIO + 1
+
+        print(f'Reading dataset (0/{numbers}) ... ', end='', flush=True)
+
         for i, line in enumerate(f):
             line = line.strip().split(',')
             content.append(line)
 
             if i % MIO == 0:
                 print(i // MIO, end=' ', flush=True)
+            
+            number_of_lines = i
         
-        print('21\nReading finished (21/21) - all 20.4 million lines read')
+        print(f'21\nReading finished ({numbers}/{numbers}) - all {round(number_of_lines / MIO, 2)} million lines read')
 
         # create table
         #table = tabulate(content, headers=headers)
 
         # create dataframe
         df = pd.DataFrame(content, columns=headers)
-        df.to_pickle('./data/minute_full_station_merged.pkl')
+        #'./data/minute_full_station_merged.pkl'
+        df.to_pickle(outputpath)
         print(df.head(20))
 
-csv_to_pickle()
+#print('reading pickle ...')
+#df = pd.read_pickle('./data/minute_full_station_merged.pkl')
+#print('reading finished')
+#print(df.head(10))
+
+#df_small = df.iloc[:2_000_000].copy()
+#df_small.to_pickle('./data/mfsm_2mio_lines.pkl')
+
+df = pd.read_pickle('./data/minute_full_station_merged.pkl')
+
+# attribute names: 'STM', 'DIR', 'Err', 'Mot', 'Osebni', 'BUS', 'LTov', 'STov', 'TTov', 'TSP', 'Vlac', 'OCC', 'GAP', 'VMIN', 'VAVG', 'VMAX', 'Time', 'FULL', 'NAME', 'SRC\n'
+
+# select numeric columns
+#numeric_cols = df.select_dtypes(include='number').columns
+
+# grouping
+#df_agg = df.groupby(df.columns[:2])[numeric_cols].sum().reset_index()
+#print(df.head(100))
+#print(df_agg.head(100))
+
+# print all the unique values for each column of the dataframe
+for col in df.columns:
+    print(col, ' - ', pd.unique(df[col]), end='\n\n')
+
+#times_unique = set()
+#for t in df['Time']:
+#    times_unique.add(t[:4])
+
+#print(times_unique)
+
+#for i in range(5):
+#    element = df.iloc[i]
+#    acc = sum(int(element[x]) for x in ['Mot', 'Osebni', 'BUS', 'LTov', 'STov', 'TTov', 'TSP', 'Vlac'])
+#    print(f'seštevek (od Mot do Vlac): {acc}, OCC: {element['OCC']}, GAP: {element['GAP']}')
+
+
